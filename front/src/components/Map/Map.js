b2c531e45dedff6c3a157dd0ed80e826f0c0d9f1
Iván Morett <ivanmorett@fb.com>
2020-06-20 17:52:53 -0500

    build

--- front/src/components/Map/Map.js
+++ front/src/components/Map/Map.js
@@ -1,12 +1,30 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
+import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+
 import './Map.scss';
 
+function MapViewer(props){
+    return(
+        <Map google={props.google} zoom={14}>
+        </Map>
+    )
+}
 
-function Map(props) {
+function MapContainer(props) {
+    // TODO: request for key
+
+    const [map, setMap] = useState(null);
+    useEffect(()=>{
+        setMap(GoogleApiWrapper({
+            apiKey: ('Key')
+        })(MapViewer));
+    }, [])
+
     return(
         <div className="Map">
+            {map}
         </div>
     )
 }
 
-export default Map;
+export default MapContainer;
